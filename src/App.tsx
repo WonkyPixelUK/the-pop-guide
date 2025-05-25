b836e31c911f4f00e700e857a308f8c10c653abb
Rich Copestake <rich@maintainhq.com>
2025-05-25 23:02:31 +0100

    Fix: Add /search route to router so search results page works. #routing #search

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,7 @@ import { Analytics } from '@vercel/analytics/react';
 import Ios from "./pages/Ios";
 import Android from "./pages/Android";
 import Api from "./pages/Api";
+import SearchPage from "./pages/Search";
 
 const queryClient = new QueryClient();
 
@@ -52,6 +53,7 @@ const App = () => (
             <Route path="/ios" element={<Ios />} />
             <Route path="/android" element={<Android />} />
             <Route path="/api" element={<Api />} />
+            <Route path="/search" element={<SearchPage />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
